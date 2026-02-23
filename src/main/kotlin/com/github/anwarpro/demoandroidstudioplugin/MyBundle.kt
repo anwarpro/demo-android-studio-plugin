2bb096b1a4a39d66262d8e8cc37a055fee54b3b1
GitHub Action <action@github.com>
2026-02-23 07:39:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/anwarpro/demoandroidstudioplugin/MyBundle.kt
+++ src/main/kotlin/com/github/anwarpro/demoandroidstudioplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.anwarpro.demoandroidstudioplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/anwarpro/demoandroidstudioplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/anwarpro/demoandroidstudioplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.anwarpro.demoandroidstudioplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
